57b1051039d64ac60d46879bb1cd6f0bd6fb0552
Pedro Javier Paneca Cordova <pjpc@ic.ufal.br>
2020-04-09 14:29:10 -0300

    Componentização

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function components() {
+  return (
+    <header>
+      <h1>ReactJS</h1>
+    </header>
+  );
+}
