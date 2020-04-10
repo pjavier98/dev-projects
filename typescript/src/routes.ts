f3115d2d841de993c9652bac1eb65d195703557b
Pedro Javier Paneca Cordova <pjpc@ic.ufal.br>
2020-04-10 10:25:31 -0300

    Tipando objetos e vetores

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
-
+import createUser from './services/CreateUser';
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'pjpc@ic.ufal.br',
+    password: '123456',
+    techs: [
+      'NodeJS',
+      'ReactJS',
+      'React Native'
+    ],
+  });
+
   return response.json({ message: 'Hello World' });
 }
